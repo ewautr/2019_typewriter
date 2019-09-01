8d9c61dbe127d103953bf911758c22d2f36ca9fb
Ewa Utracka <ewczi@Ewas-MacBook-Pro.local>
2019-09-01 20:53:41 +0200

    displaying one char at a time + sounds

--- typewriter.js
+++ typewriter.js
@@ -1,1 +1,38 @@
 "use strict";
+
+//VARIABLES
+const container = document.querySelector("#typewriter");
+const text = container.textContent.trim();
+
+//SETUP
+container.innerHTML = "";
+let i = 0;
+
+//SOUNDS
+const typeKey1 = document.getElementById("typekey1");
+const typeKey2 = document.getElementById("typekey2");
+const typeSpace = document.getElementById("typespace");
+const typeLast = document.getElementById("typelast");
+const typeReturn = document.getElementById("typereturn");
+
+function typeText() {
+  if (i < text.length) {
+    if (text.charCodeAt(i) == 32) {
+      typeSpace.play();
+    } else if (i == text.length - 1) {
+      typeLast.play();
+    } else if (text.charCodeAt(i) < 110) {
+      typeKey1.play();
+    } else if (111 < text.charCodeAt(i) < 143) {
+      typeKey2.play();
+    }
+
+    container.textContent += text[i];
+    i++;
+    setTimeout(typeText, 500);
+  } else {
+    typeReturn.play();
+  }
+}
+
+typeText();
